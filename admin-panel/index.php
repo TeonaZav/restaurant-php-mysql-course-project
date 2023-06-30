6e463326e11a14a954d707b7979bd4963a351bf6
TeonaZav <teona.zavrashvili@gmail.com>
2023-06-30 12:07:11 +0400

    feat: display statistics on admin-panel/index page

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -5,9 +5,19 @@
 <?php 
 
 $app = new App;
-
 $app->validateSessionAdminPages();
 
+$query = "SELECT COUNT(*) AS count_foods FROM foods";
+$count_foods = $app->selectOne($query);
+
+$query = "SELECT COUNT(*) AS count_orders FROM orders";
+$count_orders = $app->selectOne($query);
+
+$query = "SELECT COUNT(*) AS count_bookings FROM bookings";
+$count_bookings = $app->selectOne($query);
+
+$query = "SELECT COUNT(*) AS count_admins FROM admins";
+$count_admins = $app->selectOne($query);
 
 ?>
 
@@ -17,7 +27,7 @@ $app->validateSessionAdminPages();
             <div class="card-body">
               <h5 class="card-title">Foods</h5>
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of foods: 8</p>
+              <p class="card-text">number of foods: <?php echo $count_foods->count_foods; ?></p>
              
             </div>
           </div>
@@ -27,7 +37,7 @@ $app->validateSessionAdminPages();
             <div class="card-body">
               <h5 class="card-title">Orders</h5>
               
-              <p class="card-text">number of orders: 4</p>
+              <p class="card-text">number of orders: <?php echo $count_orders->count_orders; ?></p>
               
             </div>
           </div>
@@ -37,7 +47,7 @@ $app->validateSessionAdminPages();
             <div class="card-body">
               <h5 class="card-title">Bookings</h5>
               
-              <p class="card-text">number of bookings: 4</p>
+              <p class="card-text">number of bookings: <?php echo $count_bookings->count_bookings; ?></p>
               
             </div>
           </div>
@@ -47,7 +57,7 @@ $app->validateSessionAdminPages();
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 3</p>
+              <p class="card-text">number of admins: <?php echo $count_admins->count_admins; ?></p>
               
             </div>
           </div>
